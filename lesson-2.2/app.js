e7d7de44b9e4239fe1e1a5c3394d06ca89bffc3f
procontexta <145852331+procontexta@users.noreply.github.com>
2024-01-28 01:20:26 +0300

    Create app.js

---
+++ lesson-2.2/app.js
@@ -0,0 +1,9 @@
+let firstName = 'George'
+let age = 1
+let town = 'Moscow'
+let hobby = 'chess'
+firstName = 'Зоя'
+age = '52'
+town = 'Beirut'
+hobby = 'Маркетинг и верстка'
+console.log('Привет', firstName + ',', age + ',', town + ',', hobby + '!')
