5c046051042c328f3a8e9679a0091b3f244dde4d
procontexta <145852331+procontexta@users.noreply.github.com>
2024-01-27 20:48:50 +0300

    Delete lesson-2.2 directory

--- lesson-2.2/app.js
+++
@@ -1,9 +0,0 @@
-let firstName = 'George'
-let age = 1
-let town = 'Moscow'
-let hobby = 'chess'
-firstName = 'Зоя'
-age = '52'
-town = 'Beirut'
-hobby = 'Маркетинг, верстка'
-console.log('Привет,', firstName, ',', age, ',', town, ',', hobby, '!')
