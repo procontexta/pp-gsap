3ebff6fbbfb6e052e6428c4309fba9c94ed22449
procontexta <procontext01@gmail.com>
2024-01-28 00:25:33 +0300

    28.01-0.25

--- lesson-2.2/app.js
+++ lesson-2.2/app.js
@@ -5,5 +5,5 @@ let hobby = 'chess'
 firstName = 'Зоя'
 age = '52'
 town = 'Beirut'
-hobby = 'Маркетинг, верстка'
-console.log('Привет,', firstName, ',', age, ',', town, ',', hobby, '!')
+hobby = 'Маркетинг и верстка'
+console.log('Привет', firstName + ',', age + ',', town + ',', hobby + '!')
